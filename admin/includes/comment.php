b5db51b97364bb81c85666c806357bf4f0118d61
khoaphamctu <khoab1401150@student.ctu.edu.vn>
2017-04-18 00:08:55 +0700

    Add function find_the_comment()

--- admin/includes/comment.php
+++ admin/includes/comment.php
@@ -10,16 +10,27 @@ class Comment extends Db_object
     public $author;
     public $body;
 
-public static function create_comment($photo_id,$author = "Anh Khoa",$body =""){
-    if(!empty($photo_id) && !empty($author) && !empty($body)){
-        $comment = new Comment();
-        $comment->photo_id = (int)$photo_id;
-        $comment->author = $author;
-        $comment->body = $body;
-        return $comment;
-    } else {
-        return false;
-    }
-}
+    public static function create_comment($photo_id, $author = "Anh Khoa", $body = "")
+    {
+        if (!empty($photo_id) && !empty($author) && !empty($body)) {
+            $comment           = new Comment();
+            $comment->photo_id = (int) $photo_id;
+            $comment->author   = $author;
+            $comment->body     = $body;
+            return $comment;
+        } else {
+            return false;
+        }
+    } // end create_comment()
+
+    public static function find_the_comment($photo_id)
+    {
+        global $database;
+        $sql = "SELECT * FROM " . self::$db_name;
+        $sql .= "WHERE photo_id=". $database->escape_string($photo_id);
+        $sql .= "ORDER BY photo_id ASC";
+
+        return self::find_by_query($sql);
+    } // end find_the_comment()
 
 } //end class
