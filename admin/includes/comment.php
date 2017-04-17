7cff6583cd268843a2d5b10a41bb08597a6cf1aa
khoaphamctu <khoab1401150@student.ctu.edu.vn>
2017-04-18 00:00:58 +0700

    Add function create_comment()

--- admin/includes/comment.php
+++ admin/includes/comment.php
@@ -9,6 +9,17 @@ class Comment extends Db_object
     public $photo_id;
     public $author;
     public $body;
- 
+
+public static function create_comment($photo_id,$author = "Anh Khoa",$body =""){
+    if(!empty($photo_id) && !empty($author) && !empty($body)){
+        $comment = new Comment();
+        $comment->photo_id = (int)$photo_id;
+        $comment->author = $author;
+        $comment->body = $body;
+        return $comment;
+    } else {
+        return false;
+    }
+}
 
 } //end class
