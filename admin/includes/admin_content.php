246b012b09ac271ca652fc419fa2714b39aa7675
khoaphamctu <khoab1401150@student.ctu.edu.vn>
2017-04-29 23:31:42 +0700

    CREATE: COUNT VISITOR FUNCTION IN SESSION.PHP

--- admin/includes/admin_content.php
+++ admin/includes/admin_content.php
@@ -17,7 +17,7 @@
                                         <i class="fa fa-comments fa-5x"></i>
                                     </div>
                                     <div class="col-xs-9 text-right">
-                                        <div class="huge">26</div>
+                                        <div class="huge"><?php echo $session->count ?></div>
                                         <div>New Comments!</div>
                                     </div>
                                 </div>
--- admin/includes/session.php
+++ admin/includes/session.php
@@ -8,15 +8,26 @@ class Session
     private $signed_in = false;
     public $user_id;
     public $message;
+    public $count;
+
 
     public function __construct()
     {
         session_start();
+        $this->visitor_count();
         $this->check_the_login();
         $this->check_message();
 
+
     }
 
+    public function visitor_count(){
+        if(isset($_SESSION['count'])){
+            return $this->count = $_SESSION['count']++;
+        } else {
+            return $_SESSION['count'] = 1;
+        }
+    }
     public function is_signed_in()
     {
         return $this->signed_in;
